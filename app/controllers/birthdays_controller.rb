c634d0f45a74b087098d0dbfeaf22ba91e2f7d3d
Shane Nicholson <shanescottnicholson@gmail.com>
2021-12-02 09:13:33 -0800

    Adds routes

--- app/controllers/birthdays_controller.rb
+++ app/controllers/birthdays_controller.rb
@@ -1,2 +1,12 @@
 class BirthdaysController < ApplicationController
+  def show
+    render json: Birthday.first.as_json
+  end
+
+  def update
+    birthday = Birthday.first
+    birthday.name = "Amanda"
+    birthday.click = params[:click] || birthday.click
+    birthday.save
+  end
 end
